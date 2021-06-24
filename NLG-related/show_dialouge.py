bb77be20621026d73fc7dd3c14dec67c0d1fbf7c
oToToT <ty1208chiang@gmail.com>
2021-06-25 00:16:01 +0800

    Add dialouge display tool

---
+++ NLG-related/show_dialouge.py
@@ -0,0 +1,35 @@
+import os
+import json
+
+DATA_PATH = '../data-0614/data-0614/test_seen/'
+CHITCHAT_PATH = './nlg_output.json'
+
+g = json.load(open(CHITCHAT_PATH))
+
+o = {}
+fns = os.listdir(DATA_PATH)
+for fn in sorted(fns):
+	ds = json.load(open(DATA_PATH + fn))
+	for d in ds:
+		o[d['dialogue_id']] = {}
+		for t in d['turns']:
+			o[d['dialogue_id']][t['turn_id']] = {'speaker': t['speaker'], 'utterance': t['utterance']}
+
+def show(name):
+    for k in sorted(o[name].keys()):
+        sk = str(k)
+        if sk not in g[name].keys():
+            print(f"{o[name][k]['speaker']}: {o[name][k]['utterance']}")
+        else:
+            if len(g[name][sk]['mod']) > 0:
+                print(f"{o[name][k]['speaker']} (modified): {g[name][sk]['mod']}")
+            else:
+                if len(g[name][sk]['start']) > 0:
+	                print(f"{o[name][k]['speaker']} (chit-chat): {g[name][sk]['start']}")
+                print(f"{o[name][k]['speaker']}: {o[name][k]['utterance']}")
+                if len(g[name][sk]['end']) > 0:
+	                print(f"{o[name][k]['speaker']} (chit-chat): {g[name][sk]['end']}")
+
+print('choice:', ', '.join(o.keys()))
+name = input("which: ")
+show(name)
