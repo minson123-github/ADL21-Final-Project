2fc2d2f281e0c35c678fdef5be5397dedf58a57f
minson123 <minson123.mail@gmail.com>
2021-07-07 13:52:29 +0800

    add model downloader

---
+++ T5DST/download.sh
@@ -0,0 +1,6 @@
+wget https://www.dropbox.com/s/4fjgsib46psbb5c/model1.zip?dl=1 -O model1.zip
+wget https://www.dropbox.com/s/v3p7r60nbh1phzp/model2.zip?dl=1 -O model2.zip
+
+unzip model1.zip
+unzip model2.zip
+rm -f model1.zip model2.zip
